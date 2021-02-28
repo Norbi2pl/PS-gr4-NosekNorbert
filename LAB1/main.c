c3e4cd3761086052c69ce2be9584e218eba393e7
Norbi2pl <73993400+Norbi2pl@users.noreply.github.com>
2021-02-28 18:45:30 +0100

    Add files via upload

--- LAB1/main.c
+++ LAB1/main.c
@@ -3,6 +3,154 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //zad 1
+
+    /*
+
+    int dni = 31+29+31;
+    printf("%d",dni);
+
+    */
+
+    //zad2
+
+    /*
+    int powtorzenia = 10;
+    int wynik = 0;
+
+    for(int i = 0; i < powtorzenia; i++)
+    {
+        wynik += i;
+    }
+
+    printf("%d",wynik);
+    */
+
+    //zad3
+
+    /*
+    int powtorzenia = 10;
+    int wynik = 1;
+
+    for(int i = 1; i <= powtorzenia; i++)
+    {
+        wynik = wynik * i;
+    }
+
+    printf("%d",wynik);
+    */
+
+    //zad4
+
+    /*
+    float saldo = 1000;
+    int powtorzenia = 3;
+    float procent = 0.06;
+    float wynik = 0;
+
+    for(int i = 1; i <= powtorzenia; i++)
+    {
+        wynik = (saldo * procent) + saldo;
+        printf("%f",wynik );
+    }
+    */
+
+    //zad5
+
+
+    //zad6
+
+    /*
+    printf("   ///// \n");
+    printf(" +''''''+ \n");
+    printf("( | o o | ) \n");
+    printf("   | ^ | \n");
+    printf("  | '-' | \n");
+    printf("  +-___-+ \n");
+    */
+
+    //zad7
+
+
+    printf("**        *                  *                           *\n");
+    printf("*  *      *                  *        * * *            * * *\n");
+    printf("*   *     *   * *    * * *   *        *     *  * * *     *\n");
+    printf("*     *   * *     *  *     * *  * *   * * *    *     *   *\n");
+    printf("*       * * *     *  *       *    *   *        *         *\n");
+    printf("*         *   * *    *       * * *    * * * *  *         *\n");
+
+    printf("**        *                             *       \n");
+    printf("*  *      *                    * * *    *   *   \n");
+    printf("*   *     *   * *     * * *    *     *  *  *       \n");
+    printf("*     *   * *     *    *    *  * * *    * *       \n");
+    printf("*       * * *     *  *   *     *        *  *    \n");
+    printf("*         *   * *      * *     * * * *  *   *       \n");
+
+
+    //zad8
+
+    /*
+    printf("   + \n");
+    printf("  + + \n");
+    printf(" +   + \n");
+    printf("+-----+ \n");
+    printf("|  _  | \n");
+    printf("| ' ' | \n");
+    printf("| | | | \n");
+    printf("+-+-+-+ \n");
+    */
+
+    //zad9
+
+    /*
+    printf("         _________   [__]         ___________          \n");
+    printf("     -- (         -- * *         | Elo       |\n");
+    printf("   --    |   __   --  O   -----> | Wariat    | \n");
+    printf("*--       | |  | |               |           |\n");
+    printf("          | |  | |               |           |\n");
+    */
+
+    //zad10
+
+    /*
+    printf("Moje 3 ulubione filmy to : \n");
+    printf("Apokalipto \n");
+    printf("Avatar \n");
+    printf("Jak rozpetalem II wojne swiatowa \n");
+    */
+
+    //zad11
+
+    /*
+    printf("Godzina z nocy na dzieñ. \n");
+    printf("Godzina z boku na bok. \n");
+    printf("Godzina dla trzydziestoletnich. \n");
+    printf("Godzina uprz¹tniêta pod kogutów pianie. \n");
+    printf("Godzina, kiedy ziemia zapiera nas. \n");
+    printf("Godzina, kiedy wieje od wygas³ych gwiazd. \n");
+    printf("Godzina a-czy-po-nas-nic-nie-pozostanie. \n");
+    printf("Godzina pusta. \n");
+    printf("G³ucha, czcza. \n");
+    printf("Dno wszystkich innych godzin. \n");
+    printf("Nikomu nie jest dobrze o czwartej nad ranem. \n");
+    printf("Jeœli mrówkom jest dobrze o czwartej nad ranem \n");
+    printf("- pogratulujmy mrówkom. I niech przyjdzie pi¹ta, \n");
+    printf("o ile mamy dalej ¿yæ. \n");
+    */
+
+    //zad12
+
+    /*
+    printf("* * * * * * ===================  \n");
+    printf(" * * * * *  =================== \n");
+    printf("* * * * * * =================== \n");
+    printf(" * * * * *  =================== \n");
+    printf("* * * * * * =================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    */
+
     return 0;
 }
