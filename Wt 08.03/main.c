58c9f192f22562ad8857f3c8a70045c1a31decdc
Norbi <norbert.nosek@student.uwm.edu.pl>
2021-03-11 20:53:00 +0100

    Komentarz 1

---
+++ Wt 08.03/main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int n, m;
+
+    printf("Podaj liczbe calkowita n : ");
+    scanf("%d", &n);
+
+    printf("Podaj liczbe calkowita m : ");
+    scanf("%d", &m);
+
+    for(int i = n; i < m; i += n)
+    {
+        printf("%d\n", i);
+    }
+    */
+
+    //zad3
+
+    /*
+    int n, m, k;
+
+    printf("Podaj liczbe calkowita n wieksza od 0 : ");
+    scanf("%d", &n);
+
+    printf("Podaj liczbe calkowita m wieksza od 0 : ");
+    scanf("%d", &m);
+
+    printf("Podaj liczbe calkowita k wieksza od 0 : ");
+    scanf("%d", &k);
+
+    if(n > 0 && m > 0 && k > 0)
+    {
+        return 1;
+    }
+    */
+
+    //zad4
+
+    /*
+    int n, silnia = 1;
+
+    printf("Podaj liczbe calkowita n wieksza od 0 : ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        silnia *= i;
+        printf("%d\n", silnia);
+    }
+    */
+
+    //zad5
+
+    /*
+    int n, suma = 0;
+    int dzialanie = 0;
+
+    printf("Podaj liczbe calkowita n : ");
+    scanf("%d", &n);
+
+    for(int i = 0; i <= n; i++)
+    {
+        dzialanie = i*i;
+        suma += dzialanie;
+    }
+    printf("%d\n", suma);
+    */
+
+    //zad6
+
+    /*
+    int n, q = 2, wynik = 1;
+
+    printf("Podaj liczbe calkowita n wieksza od 2 : ");
+    scanf("%d", &n);
+
+    if(n < 2)
+    {
+        return 1;
+    }
+
+    for(int i = 2; i <= n; i+=2)
+    {
+        wynik *= i;
+    }
+    printf("%d", wynik);
+    */
+
+    //zad7
+
+    /*
+    int n, m, iloczyn = 1;
+
+    printf("Podaj liczbe calkowita n : ");
+    scanf("%d", &n);
+    printf("Podaj liczbe calkowita m : ");
+    scanf("%d", &m);
+
+    if(n > m)
+    {
+        return 1;
+    }
+    for(int i = n; i <= m; i++)
+    {
+        iloczyn
+    }
+    */
+
+    //zad11
+
+    int a, b, c, d, x;
+
+    printf("Podaj liczbe calkowita a : ");
+    scanf("%d", &a);
+    printf("Podaj liczbe calkowita b : ");
+    scanf("%d", &b);
+    printf("Podaj liczbe calkowita c : ");
+    scanf("%d", &c);
+    printf("Podaj liczbe calkowita d : ");
+    scanf("%d", &d);
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ Wtorek-01.03/main.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //zad1
+
+    /*
+    printf("Hello world!\n");
+    */
+
+    //zad2
+
+    /*
+    printf("Bardzo \n");
+    printf("dlugi \n");
+    printf("napis \n");
+    */
+
+    //zad3
+
+    /*
+    printf("Napis zawierajacy rozne dziwne znaki // \\\\ $ & % \n");
+    */
+
+
+    //zad4
+
+    /*
+    int a = 4;
+    printf("%d",a);
+    */
+
+    //zad5
+
+    /*
+    float b = 1.5;
+    printf("%f",b);
+    */
+
+    //zad6
+
+    /*
+    int a = 2, b = 3, c = 4;
+    printf("%d \n",a);
+    printf("%d \n",b);
+    printf("%d \n",c);
+    */
+
+    //zad7
+
+    /*
+    int a = 2;
+    printf("%d",a + 1);
+    */
+
+    //zad8
+
+    /*
+    int a = 3, b = 4, c = 5;
+    printf("%d", (a + b + c)/3);
+    */
+
+    //zad9
+
+    /*
+    double a = 4.5;
+    printf("%f",sqrt(a));
+    */
+
+    //zad10
+
+    /*
+    double a = -4;
+    printf("%f",fabs(a));
+    */
+
+    //zad11
+
+    /*
+    float a = 1.5678;
+    printf("%.2f",a);
+    */
+
+    //zad12
+
+    /*
+    float a;
+    scanf("%f", &a);
+    printf("%e",a);
+    */
+
+    //Instrukcje Warunkowe
+
+    //zad1
+
+    /*
+    int a;
+    scanf("%d", &a);
+    printf("%d",abs(a));
+    */
+
+    //zad2
+
+    /*
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    if(a>b)
+    {
+        printf("%d", a);
+    }
+    else
+    {
+        printf("%d", b);
+    }
+    else
+    {
+        printf("%d", a);
+        printf("%d", b);
+    }
+    */
+
+    //zad3
+
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+
+
+
+
+
+    return 0;
+}
