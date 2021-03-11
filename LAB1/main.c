fcf9e0fd7bf018371a11e091cc2b9aaf5b037f16
Norbi <norbert.nosek@student.uwm.edu.pl>
2021-03-11 20:57:13 +0100

    Kom

--- LAB1/main.c
+++ LAB1/main.c
@@ -1,3 +1,249 @@
+<<<<<<< HEAD
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //zad 1
+
+    /*
+
+    int dni = 31+29+31;
+    printf("%d",dni);
+
+    */
+
+    //zad2
+
+    /*
+    int powtorzenia = 10;
+    int wynik = 0;
+
+    for(int i = 0; i < powtorzenia; i++)
+    {
+        wynik += i;
+    }
+
+    printf("%d",wynik);
+    */
+
+    //zad3
+
+    /*
+    int powtorzenia = 10;
+    int wynik = 1;
+
+    for(int i = 1; i <= powtorzenia; i++)
+    {
+        wynik = wynik * i;
+    }
+
+    printf("%d",wynik);
+    */
+
+    //zad4
+
+    /*
+    float saldo = 1000;
+    int powtorzenia = 3;
+    float procent = 0.06;
+    float wynik = 0;
+
+    for(int i = 1; i <= powtorzenia; i++)
+    {
+        wynik = (saldo * procent) + saldo;
+        printf("%f",wynik );
+    }
+    */
+
+    //zad5
+
+
+    //zad6
+
+    /*
+    printf("   ///// \n");
+    printf(" +''''''+ \n");
+    printf("( | o o | ) \n");
+    printf("   | ^ | \n");
+    printf("  | '-' | \n");
+    printf("  +-___-+ \n");
+    */
+
+    //zad7
+
+
+    printf("**        *                  *                           *\n");
+    printf("*  *      *                  *        * * *            * * *\n");
+    printf("*   *     *   * *    * * *   *        *     *  * * *     *\n");
+    printf("*     *   * *     *  *     * *  * *   * * *    *     *   *\n");
+    printf("*       * * *     *  *       *    *   *        *         *\n");
+    printf("*         *   * *    *       * * *    * * * *  *         *\n");
+
+    printf("**        *                             *       \n");
+    printf("*  *      *                    * * *    *   *   \n");
+    printf("*   *     *   * *     * * *    *     *  *  *       \n");
+    printf("*     *   * *     *    *    *  * * *    * *       \n");
+    printf("*       * * *     *  *   *     *        *  *    \n");
+    printf("*         *   * *      * *     * * * *  *   *       \n");
+
+
+    //zad8
+
+    /*
+    printf("   + \n");
+    printf("  + + \n");
+    printf(" +   + \n");
+    printf("+-----+ \n");
+    printf("|  _  | \n");
+    printf("| ' ' | \n");
+    printf("| | | | \n");
+    printf("+-+-+-+ \n");
+    */
+
+    //zad9
+
+    /*
+    printf("         _________   [__]         ___________          \n");
+    printf("     -- (         -- * *         | Elo       |\n");
+    printf("   --    |   __   --  O   -----> | Wariat    | \n");
+    printf("*--       | |  | |               |           |\n");
+    printf("          | |  | |               |           |\n");
+    */
+
+    //zad10
+
+    /*
+    printf("Moje 3 ulubione filmy to : \n");
+    printf("Apokalipto \n");
+    printf("Avatar \n");
+    printf("Jak rozpetalem II wojne swiatowa \n");
+    */
+
+    //zad11
+
+    /*
+    printf("Godzina z nocy na dzieñ. \n");
+    printf("Godzina z boku na bok. \n");
+    printf("Godzina dla trzydziestoletnich. \n");
+    printf("Godzina uprz¹tniêta pod kogutów pianie. \n");
+    printf("Godzina, kiedy ziemia zapiera nas. \n");
+    printf("Godzina, kiedy wieje od wygas³ych gwiazd. \n");
+    printf("Godzina a-czy-po-nas-nic-nie-pozostanie. \n");
+    printf("Godzina pusta. \n");
+    printf("G³ucha, czcza. \n");
+    printf("Dno wszystkich innych godzin. \n");
+    printf("Nikomu nie jest dobrze o czwartej nad ranem. \n");
+    printf("Jeœli mrówkom jest dobrze o czwartej nad ranem \n");
+    printf("- pogratulujmy mrówkom. I niech przyjdzie pi¹ta, \n");
+    printf("o ile mamy dalej ¿yæ. \n");
+    */
+
+    //zad12
+
+    /*
+    printf("* * * * * * ===================  \n");
+    printf(" * * * * *  =================== \n");
+    printf("* * * * * * =================== \n");
+    printf(" * * * * *  =================== \n");
+    printf("* * * * * * =================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    printf("=============================== \n");
+    */
+
+
+    //WTOREK 01.03
+
+    //zad1
+
+    /*
+    printf("Hello world!\n");
+    */
+
+    //zad2
+
+    /*
+    printf("Bardzo \n");
+    printf("dlugi \n");
+    printf("napis \n");
+    */
+
+    //zad3
+
+    /*
+    printf("Napis zawierajacy rozne dziwne znaki // \\\\ $ & % \n");
+    */
+
+
+    //zad4
+
+    /*
+    int a = 4;
+    printf("%d",a);
+    */
+
+    //zad5
+
+    /*
+    float b = 1.5;
+    printf("%f",b);
+    */
+
+    //zad6
+
+    /*
+    int a = 2, b = 3, c = 4;
+    printf("%d \n",a);
+    printf("%d \n",b);
+    printf("%d \n",c);
+    */
+
+    //zad7
+
+    /*
+    int a = 2;
+    printf("%d",a + 1);
+    */
+
+    //zad8
+
+    /*
+    int a = 3, b = 4, c = 5;
+    printf("%d", (a + b + c)/3);
+    */
+
+    //zad9
+
+    /*
+    double a = 4.5;
+    printf("%f",sqrt(a));
+    */
+
+    //zad10
+
+    /*
+    double a = -4;
+    printf("%f",fabs(a));
+    */
+
+    //zad11
+
+    /*
+    float a = 1.5678;
+    printf("%.2f",a);
+    */
+
+    //zad12
+
+    float a = 0.2;
+    printf("%e",a);
+
+
+    return 0;
+}
+=======
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -154,3 +400,4 @@ int main()
 
     return 0;
 }
+>>>>>>> be59df1462a23dd35f451f3d2af6de9d3acb4434
