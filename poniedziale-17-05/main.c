150b835be41aca5adc1077dd169d9c9763411886
Norbi <norbert.nosek@student.uwm.edu.pl>
2021-06-08 11:29:44 +0200

    comit

--- poniedziale-17-05/main.c
+++ poniedziale-17-05/main.c
@@ -4,17 +4,49 @@
 struct trojkat
 {
     double a, b, c;
-};
+}trojkat;
 
 double obwod(struct trojkat t)
 {
-    return t .a+t .b+t .c;
+    return t.a + t.b + t.c;
+}
+
+typedef struct zespolone
+{
+    double re, im;
+
+} zespolone;
+
+zespolone dodaj(zespolone war1, zespolone war2)
+{
+    zespolone wynik = {war1.re + war2.re, war1.im + war2.im};
+    return wynik;
+}
+
+
+typedef struct student{
+    char imie[50];
+    char nazwisko[50];
+    char adres[50];
+    int pesel[11];
+} student;
+
+void info_student(student *s)
+{
+    printf("Imie studenta: ");
+    gets(s->imie);
+    printf("Nazwisko studenta: ");
+    gets(s->nazwisko);
+    printf("Adres studenta: ");
+    gets(s->adres);
+    printf("Pesel studenta: ");
+    gets(s->pesel);
 }
 
 
 typedef enum zwierzak
 {
-    kot = 0,
+    kot,
     pies,
     chomik,
     ryba,
@@ -24,7 +56,7 @@ typedef enum zwierzak
 
 int main()
 {
-    zwierzak zwierz = ryba;
+    zwierzak zwierz = kot;
     printf("Numer zwierzaka %d\n", (int)zwierz);
 
     return 0;
